08c1e789eaebbc2916fc14b0f1e6e9eec7204d01
张琳 <zhanglin@waction.cn>
2017-11-20 18:08:46 +0800

    deploy to server

--- ulb_manager/wsgi.py
+++ ulb_manager/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ulb_manager.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ulb_manager.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inner_ulb_manager.prod")
 
 application = get_wsgi_application()
